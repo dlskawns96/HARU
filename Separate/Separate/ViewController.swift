23205ded7fb9f58298a1e11b515151e8d35dfaf1
LeeNamJun <dlskawns96@gmail.com>
2020-12-18 19:18:19 +0900

    Created project

---
+++ Separate/Separate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Separate
+//
+//  Created by Lee Nam Jun on 2020/12/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
