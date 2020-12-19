6863e26eacd2f4f03a60bde617b4f1c23b706e20
LeeNamJun <dlskawns96@gmail.com>
2020-12-19 11:42:18 +0900

    Show Schedule Setter VC as Modally with navigation bar

--- Separate/Separate/ViewControllers/HomeVC.swift
+++ Separate/Separate/ViewControllers/HomeVC.swift
@@ -1,19 +1,21 @@
 //
-//  ViewController.swift
+//  HomeVC.swift
 //  Separate
 //
 //  Created by Lee Nam Jun on 2020/12/18.
 //
 
+import Foundation
 import UIKit
 
-class ViewController: UIViewController {
-
+class HomeVC: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        navigationItem.title = "Title"
+    }
 }
-
---
+++ Separate/Separate/ViewControllers/ScheduleSetVC.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Separate
+//
+//  Created by Lee Nam Jun on 2020/12/18.
+//
+
+import UIKit
+
+class ScheduleSetVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        navigationItem.title = "Title"
+    }
+}
+
