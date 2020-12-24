e3e07a4c9b14a302570a34c49cccf2b2a20188dd
LeeNamJun <dlskawns96@gmail.com>
2020-12-24 16:04:56 +0900

    Project Created

---
+++ HARU/HARU/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HARU
+//
+//  Created by Lee Nam Jun on 2020/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
