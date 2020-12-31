a00e9d50af0808d42faa52f6c41f17d6c2de7d8c
Chosiyeon <chosiyeonn@gmail.com>
2020-12-31 18:18:10 +0900

    Update Calendar from HARU

---
+++ HARU/HARU/Calendar.swift
@@ -0,0 +1,25 @@
+//
+//  Calendar.swift
+//  HARU
+//
+//  Created by Cho Si Yeon on 2020/12/31.
+//
+
+import Foundation
+
+class Calendar {
+    
+    var title : String
+    var startDate : String
+    var endDate : String
+    var identifier : String
+    
+    init(title : String, startDate : String, endDate : String, indentifier : String){
+        
+        self.title = title
+        self.startDate = startDate
+        self.endDate = endDate
+        self.identifier = indentifier
+        
+    }
+}
--- HARU/HARU/ViewControllers/SelectDateController.swift
+++ HARU/HARU/ViewControllers/SelectDateController.swift
@@ -17,17 +17,20 @@ class SelectDateController : UIViewController{
     @IBAction func closeBtn(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
     }
-    
-//    @IBAction func addBtn(_ sender: Any) {
-//        guard let controller = self.storyboard?.instantiateViewController(identifier: "AddScheduleController") as? AddScheduleController else { return }
-//
-//        self.present(controller, animated: true, completion: nil)
-//    }
-    
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         self.date.text = paramDate
-        
     }
+    
+    // 해당 날짜의 전체 일정 보여주기
+    
+    // 특정 일정 눌렀을 때 그 일정에 대한 세부 정보 표시
+    
+    // 특정 일정 눌렀을 때 삭제 버튼 추가
+    
+    // 삭제 버튼 누르면 기본 캘린더에서도 삭제 되게
+    
+    // 상단부는 일정 표시, 아래는 일기
     
 }
