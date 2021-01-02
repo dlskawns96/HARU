e271fddde6be659fbeaa043ff99f21b43666d778
LeeNamJun <dlskawns96@gmail.com>
2021-01-02 14:40:31 +0900

    import AFDateHelper

--- HARU/Pods/Target Support Files/Pods-HARU/Pods-HARU-frameworks.sh
+++ HARU/Pods/Target Support Files/Pods-HARU/Pods-HARU-frameworks.sh
@@ -175,9 +175,11 @@ code_sign_if_enabled() {
 }
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/AFDateHelper/AFDateHelper.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/FSCalendar/FSCalendar.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/AFDateHelper/AFDateHelper.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/FSCalendar/FSCalendar.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
