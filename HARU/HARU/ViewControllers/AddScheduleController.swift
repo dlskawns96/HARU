11a3feaff494848cff0377a36a25e151f258ae82
LeeNamJun <dlskawns96@gmail.com>
2021-01-11 16:09:38 +0900

    Create Event add view as table view

--- HARU/HARU/ViewControllers/AddScheduleController.swift
+++ HARU/HARU/ViewControllers/AddScheduleController.swift
@@ -8,7 +8,7 @@
 import UIKit
 import EventKit
 
-class AddScheduleController : UIViewController {
+class AddScheduleController : UITableViewController {
   
     var savedEventId : String = ""
     var hTitle : String = "Test"
