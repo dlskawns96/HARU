feee67ed253288d68de1e9a390c28a7e5ab9737f
Chosiyeon <chosiyeonn@gmail.com>
2021-01-22 23:31:41 +0900

    Set default UI

--- HARU/HARU/ViewControllers/SelectDateController.swift
+++ HARU/HARU/ViewControllers/SelectDateController.swift
@@ -7,94 +7,37 @@
 
 import UIKit
 
-class SelectDateController : UIViewController, UITableViewDataSource, UITableViewDelegate {
+class SelectDateController : UIViewController {
     
     
     @IBOutlet weak var segment: UISegmentedControl!
-    @IBOutlet weak var tableView: UITableView!
     
-    var count: Int = HCalendar.calendarList.count
-    var token: NSObjectProtocol?
-    
-    let AD = UIApplication.shared.delegate as? AppDelegate
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        CoreDataManager.shared.fetchDiary()
-        tableView.reloadData()
-    }
-    
-    deinit {
-        if let token = token {
-            NotificationCenter.default.removeObserver(token)
-        }
-    }
+    @IBOutlet weak var scheduleView: UIView!
+    @IBOutlet weak var diaryView: UIView!
+
     override func viewDidLoad() {
-        
         super.viewDidLoad()
-        self.tableView.dataSource = self
-        self.tableView.delegate = self
-        
-        token = NotificationCenter.default.addObserver(forName: AddDiaryController.newDiary, object: nil, queue: OperationQueue.main) {_ in
-            self.reloadTableView()
-            print("reload")
-        }
-        
-    }
-    
-    func reloadTableView(){
-        
-        switch segment.selectedSegmentIndex
-        {
-        case 0:
-            count = HCalendar.calendarList.count
-            tableView.reloadData()
-        case 1:
-            count = CoreDataManager.returnDiaryCount(date: (AD?.selectedDate)!)
-            tableView.reloadData()
-        default:
-            break
-        }
-        
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
-        return count
-        
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        
-        switch segment.selectedSegmentIndex
-        {
-        case 0:
-            let target = HCalendar.calendarList[indexPath.row]
-            cell.textLabel?.text = target.title
-        case 1:
-            print(CoreDataManager.returnDiary(date: (AD?.selectedDate)!))
-            let row = indexPath.row
-            let list = CoreDataManager.returnDiary(date: (AD?.selectedDate)!)
-            cell.textLabel?.text = list[row]
-        default:
-            break
-        }
-        
-        return cell
+        scheduleView.isHidden = false
+        diaryView.isHidden = true
     }
     
     @IBAction func indexChanged(_ sender: Any) {
-        reloadTableView()
+        switch segment.selectedSegmentIndex
+        {
+        case 0:
+            scheduleView.isHidden = false
+            diaryView.isHidden = true
+        case 1:
+            scheduleView.isHidden = true
+            diaryView.isHidden = false
+        default:
+            break
+        }
     }
-    
-    
     
     @IBAction func closeBtn(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
     }
-    
     
     @IBAction func addBtn(_ sender: Any) {
         
@@ -113,16 +56,5 @@ class SelectDateController : UIViewController, UITableViewDataSource, UITableVie
         }
         
     }
-    
-    
-    // 해당 날짜의 전체 일정 보여주기
-    
-    // 특정 일정 눌렀을 때 그 일정에 대한 세부 정보 표시
-    
-    // 특정 일정 눌렀을 때 삭제 버튼 추가
-    
-    // 삭제 버튼 누르면 기본 캘린더에서도 삭제 되게
-    
-    // 상단부는 일정 표시, 아래는 일기
     
 }
