7a006cb02d10cfcfdc4a2786c64bc956828170c3
LeeNamJun <dlskawns96@gmail.com>
2021-02-05 15:42:50 +0900

    Swipe action 추가

--- HARU/HARU/ViewControllers/ScheduleViewController.swift
+++ HARU/HARU/ViewControllers/ScheduleViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ScheduleViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class ScheduleViewController: UIViewController {
 
     @IBOutlet weak var ScheduleTableView: UITableView!
     var dateEvents: [NewEvent] = []
@@ -19,6 +19,16 @@ class ScheduleViewController: UIViewController, UITableViewDelegate, UITableView
         ScheduleTableView.removeExtraLine()
     }
     
+    func swipeDelete() {
+        print("ScheduleViewController - swipeDelete() called")
+    }
+    
+    func swipeModify() {
+        print("ScheduleViewController - swipeModify() called")
+    }
+}
+
+extension ScheduleViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return dateEvents.count
     }
@@ -41,6 +51,28 @@ class ScheduleViewController: UIViewController, UITableViewDelegate, UITableView
     
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
+    }
+    
+    // leading swipe
+    func tableView(_ tableView: UITableView, leadingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let action = UIContextualAction(style: .destructive, title: "삭제", handler: {(action, view, completionHandler) in
+            self.swipeDelete()
+            completionHandler(true)
+        })
+        action.backgroundColor = .systemRed
+        
+        return UISwipeActionsConfiguration(actions: [action])
+    }
+    
+    // trailing swipe
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let action = UIContextualAction(style: .normal, title: "수정", handler: {(action, view, completionHandler) in
+            self.swipeModify()
+            completionHandler(true)
+        })
+        action.backgroundColor = .systemBlue
+        
+        return UISwipeActionsConfiguration(actions: [action])
     }
 }
 
