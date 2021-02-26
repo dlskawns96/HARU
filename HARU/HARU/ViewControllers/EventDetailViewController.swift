9f6f6d08123a4cf88868a5d8f179aeda87d61cff
LeeNamJun <dlskawns96@gmail.com>
2021-02-26 17:35:02 +0900

    일정 디테일에서 일정 수정

--- HARU/HARU/ViewControllers/EventDetailViewController.swift
+++ HARU/HARU/ViewControllers/EventDetailViewController.swift
@@ -9,25 +9,55 @@ import UIKit
 import EventKit
 
 class EventDetailViewController: UIViewController {
-
+    
     var eventStore = EKEventStore()
     var event: EKEvent!
     var dateFormatter = DateFormatter()
     
     @IBOutlet weak var tableView: UITableView!
     
+    static let eventChangedNoti = Notification.Name(rawValue: "eventChangedNoti")
+    var token: NSObjectProtocol?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         dateFormatter.locale = Locale(identifier: "ko-KR")
         dateFormatter.dateFormat = "yyyy년 MM월 dd일 EEEE a HH시 mm분"
         tableView.delegate = self
         tableView.dataSource = self
         // Do any additional setup after loading the view.
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(self.getNewEventInfo(_:)), name: EventDetailViewController.eventChangedNoti, object: nil)
     }
     
     @IBAction func backBtnClicked(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func editBtnClicked(_ sender: Any) {
+        guard let controller = storyboard?.instantiateViewController(withIdentifier: "EventModifyViewControllerEntry") as? UINavigationController else { return }
+        
+        guard let vc = controller.viewControllers.first as? EventModifyViewController else { return }
+        vc.event = (event.copy() as! EKEvent)
+        vc.originalCalendar = event.calendar
+        
+        controller.modalPresentationStyle = .pageSheet
+        self.present(controller, animated: true, completion: nil)
+    }
+    
+    deinit {
+        if let token = token {
+            NotificationCenter.default.removeObserver(token)
+        }
+    }
+    
+    @objc
+    func getNewEventInfo(_ notification: NSNotification) {
+        if let newEvent = notification.userInfo?["EKEvent"] as? EKEvent {
+            event = newEvent
+            tableView.reloadData()
+        }
     }
 }
 
@@ -42,7 +72,7 @@ extension EventDetailViewController: UITableViewDelegate, UITableViewDataSource 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         if indexPath.row == 0 {
             guard let cell = tableView.dequeueReusableCell(withIdentifier: "EventDetailTableViewCell") as? EventDetailTableViewCell else { return UITableViewCell() }
-
+            
             cell.eventTitleLabel.text = event.title
             cell.startDateLabel.text = dateFormatter.string(from: event.startDate)
             cell.endDateLabel.text = "~" + dateFormatter.string(from: event.endDate)
@@ -51,7 +81,7 @@ extension EventDetailViewController: UITableViewDelegate, UITableViewDataSource 
         } else {
             guard let cell = tableView.dequeueReusableCell(withIdentifier: "ButtonTableViewCell") as? ButtonTableViewCell else {
                 return UITableViewCell() }
-
+            
             if indexPath.row == 1 {
                 cell.buttonTitleLabel.text = "캘린더"
                 cell.buttonContentLabel.text = event.calendar.title
@@ -70,5 +100,9 @@ extension EventDetailViewController: UITableViewDelegate, UITableViewDataSource 
             
             return cell
         }
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        return
     }
 }
