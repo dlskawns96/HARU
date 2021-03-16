75722c0267f586eaf5345fcacdd82a1eb0dc0d7d
조시연 <chosiyeonn@gmail.com>
2021-03-16 22:03:30 +0900

    다이어리 뷰 멘트 추가

--- HARU/HARU/ViewControllers/DiaryViewController.swift
+++ HARU/HARU/ViewControllers/DiaryViewController.swift
@@ -48,7 +48,9 @@ class DiaryViewController: UIViewController, UIGestureRecognizerDelegate, UIPick
     
     func setComment() {
         
-        let comments = ["지나간 하루는 돌아오지 않아요!", "지난날에 대한 후회는 하지 마세요!", "이미 지나버렸어요"]
+        let comments = ["지나간 하루는 돌아오지 않아요!", "지난날에 대한 후회는 하지 마세요!", "이미 지나버렸어요", "앞으로는 바로바로 기록해요!"]
+        let able_comments = ["오늘 하루는 어땠나요?", "오늘 하루를 기록하세요!", "오늘의 기록을 추가하세요!"]
+        
         let dateString = AD?.selectedDate
         dateFormatter.dateFormat = "yyyy-MM-dd"
         
@@ -60,7 +62,7 @@ class DiaryViewController: UIViewController, UIGestureRecognizerDelegate, UIPick
             }
             else {
                 centerLabel.isHidden = false
-                centerLabel.text = "오늘 하루를 기록하세요!"
+                centerLabel.text = able_comments.randomElement()
             }
         }
         else {
