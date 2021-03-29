2badb4358acfe1c4ad85f3787a40053c81a718c5
LeeNamJun <dlskawns96@gmail.com>
2021-03-29 19:37:10 +0900

    Event 삭제 후 메인 캘린더에서 반영이 안되는 이슈 해결

--- HARU/HARU/Classes/EventHandler.swift
+++ HARU/HARU/Classes/EventHandler.swift
@@ -10,6 +10,7 @@ import EventKit
 
 class EventHandler {
     static var ekEventStore: EKEventStore?
+    static let eventRemovedNoti = Notification.Name("eventRemovedNoti")
     var event: EKEvent? = nil
     
     init() {
@@ -21,6 +22,7 @@ class EventHandler {
             print("Event to remove", event)
             let ev = EventHandler.ekEventStore?.event(withIdentifier: event.eventIdentifier)
             try EventHandler.ekEventStore!.remove(ev!, span: .thisEvent, commit: true)
+            NotificationCenter.default.post(name: EventHandler.eventRemovedNoti, object: nil, userInfo: ["startDate": event.startDate, "endDate": event.endDate])
             print("event removed")
             return true
         } catch {
