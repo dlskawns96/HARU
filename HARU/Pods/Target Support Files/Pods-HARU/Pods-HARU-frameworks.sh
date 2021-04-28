126ce8abb9ecd5166656de1978e6c6340a08e857
LeeNamJun <dlskawns96@gmail.com>
2021-04-29 00:47:58 +0900

    Pod 수정

--- HARU/Pods/Target Support Files/Pods-HARU/Pods-HARU-frameworks.sh
+++ HARU/Pods/Target Support Files/Pods-HARU/Pods-HARU-frameworks.sh
@@ -175,14 +175,14 @@ code_sign_if_enabled() {
 }
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/AFDateHelper/AFDateHelper.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/DropDown/DropDown.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/FSCalendar/FSCalendar.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/AFDateHelper-framework/AFDateHelper.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/DropDown-framework/DropDown.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/FSCalendar-framework/FSCalendar.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/AFDateHelper/AFDateHelper.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/DropDown/DropDown.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/FSCalendar/FSCalendar.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/AFDateHelper-framework/AFDateHelper.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/DropDown-framework/DropDown.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/FSCalendar-framework/FSCalendar.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
