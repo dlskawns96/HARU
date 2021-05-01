263052b30fa465896d5ce21f9c2d805b15b7216d
LeeNamJun <dlskawns96@gmail.com>
2021-05-02 00:22:25 +0900

    Add Test Push Notification

--- HARU/HARU/AppDelegate.swift
+++ HARU/HARU/AppDelegate.swift
@@ -12,24 +12,26 @@ import EventKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
     var selectedDate: String?
     var selectedDateEvents: [EKEvent]?  // 선택한 날짜의 이벤트들 저장
+    let userNotificationCenter = UNUserNotificationCenter.current()
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        registerForRemoteNotifications()
+        sendNotification(seconds: 60)
         return true
     }
-
+    
     // MARK: UISceneSession Lifecycle
-
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
-
+    
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
@@ -40,6 +42,72 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationDidFinishLaunching(_ application: UIApplication) {
         DropDown.startListeningToKeyboard()
     }
+    
+    private func registerForRemoteNotifications() {
+        
+        // 1. 푸시 center (유저에게 권한 요청 용도)
+        let center = UNUserNotificationCenter.current()
+        center.delegate = self // push처리에 대한 delegate - UNUserNotificationCenterDelegate
+        let options: UNAuthorizationOptions = [.alert, .sound, .badge]
+        center.requestAuthorization(options: options) { (granted, error) in
+            
+            guard granted else {
+                return
+            }
+            
+            DispatchQueue.main.async {
+                // 2. APNs에 디바이스 토큰 등록
+                UIApplication.shared.registerForRemoteNotifications()
+                print("@@@")
+            }
+        }
+    }
+    
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        // 토큰 등록 성공 시 호출
+    }
+    
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        // 토큰 등록 실패 시 호출
+    }
+    
+    func sendNotification(seconds: Double) {
+        let notificationContent = UNMutableNotificationContent()
 
+        notificationContent.title = "알림 테스트"
+        notificationContent.body = "이것은 알림을 테스트 하는 것이다"
+
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: seconds, repeats: true)
+        let request = UNNotificationRequest(identifier: "testNotification",
+                                            content: notificationContent,
+                                            trigger: trigger)
+
+        userNotificationCenter.add(request) { error in
+            if let error = error {
+                print("Notification Error: ", error)
+            }
+        }
+    }
 }
 
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    
+    // 3. 앱이 foreground상태 일 때, 알림이 온 경우 어떻게 표현할 것인지 처리
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                willPresent notification: UNNotification,
+                                withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        
+        // 푸시가 오면 alert, badge, sound표시를 하라는 의미
+        completionHandler([.banner, .badge, .sound])
+    }
+    
+    // push가 온 경우 처리
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        
+        // deep link처리 시 아래 url값 가지고 처리
+        let url = response.notification.request.content.userInfo
+        print("url = \(url)")
+        
+        // if url.containts("receipt")...
+    }
+}
---
+++ HARU/HARU/Classes/PushNotiHandler.swift
@@ -0,0 +1,11 @@
+//
+//  PushNotiHandler.swift
+//  HARU
+//
+//  Created by Lee Nam Jun on 2021/05/01.
+//
+
+import Foundation
+
+class PushNotiHandler {
+}
